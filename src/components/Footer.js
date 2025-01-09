82da5293b1a948c61235dbea3dc108d1ceb27596
muskan <mprajapati1408@gmail.com>
2025-01-09 17:05:09 +0530

    error_solve

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import './Footer.css';
 
 export default function Footer() {
   return (
-    <MDBFooter bgColor='dark' className='text-center text-lg-start text-muted'>
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         <div className='me-5 d-none d-lg-block'>
           <span>Get connected with us on social networks:</span>
